b1d81c04d139f68e83e1d2d75a0041966cbb6cdc
Wendly Saintil <wendly@wendly-macbookpro2.roam.corp.google.com>
2019-06-07 08:34:38 -0700

    Update express starter config package

--- dist/index.js
+++ dist/index.js
@@ -4,18 +4,8 @@ const landing_1 = require("./lib/routes/landing");
 const express_starter_config_1 = require("express-starter-config");
 class ApplicationServer extends express_starter_config_1.BaseApplicationServer {
     constructor() {
-        super({
-            port: 1000,
-            publicPathHandler: (req, res, next) => {
-                console.log('Pre public');
-                next();
-            }
-        });
+        super();
         this.addControllers(landing_1.LandingController);
-        this.app.get('/blog', (req, res) => res.send('Blog'));
-        this.app.get('/blog/:id', (req, res) => {
-            res.send(`Blog ${req.params.id}`);
-        });
     }
 }
 // Start the application
